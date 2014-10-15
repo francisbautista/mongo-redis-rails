83c61079bc33f22c46047ffd9373306acb4e53ba
Francis Bautista <francis.bautista07@gmail.com>
2014-10-15 23:24:56 +0800

    Repair for mongoid config by regen yml

--- test/helpers/categories_helper_test.rb
+++ test/helpers/categories_helper_test.rb
@@ -1,4 +1,5 @@
 require 'test_helper'
+require 'rails/mongoid'
 
 class CategoriesHelperTest < ActionView::TestCase
 end
--- test/helpers/customers_helper_test.rb
+++ test/helpers/customers_helper_test.rb
@@ -1,4 +1,5 @@
 require 'test_helper'
+require 'rails/mongoid'
 
 class CustomersHelperTest < ActionView::TestCase
 end
